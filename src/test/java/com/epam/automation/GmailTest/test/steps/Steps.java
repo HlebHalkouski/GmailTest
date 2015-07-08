8f3c51341aa2b5cfb0851913f4afcd93b4ffb147
HlebHalkouski <dr71gleb@gmail.com>
2015-07-08 18:50:52 +0300

    commit

---
+++ src/test/java/com/epam/automation/GmailTest/test/steps/Steps.java
@@ -0,0 +1,8 @@
+package com.epam.automation.GmailTest.test.steps;
+
+import com.epam.automation.GmailTest.webdriver.BaseEntity;
+
+public class Steps extends BaseEntity {
+
+	
+}
---
+++ src/test/java/com/epam/automation/GmailTest/webdriver/elements/TextBox.java
@@ -0,0 +1,5 @@
+package com.epam.automation.GmailTest.webdriver.elements;
+
+public class TextBox {
+
+}
